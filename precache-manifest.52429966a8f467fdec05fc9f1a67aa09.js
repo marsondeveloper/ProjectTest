9280e74049678a436cb6433fdaa0976ca70d8d49
удвщкфвщ <marson4ik2019@gmail.com>
2020-03-21 02:24:40 +0200

    Updates

---
+++ precache-manifest.52429966a8f467fdec05fc9f1a67aa09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d082cbed63a94b6d2028b52a640889",
+    "url": "/TestProject/index.html"
+  },
+  {
+    "revision": "6ffb1809fba552e8302c",
+    "url": "/TestProject/static/css/main.438bf3e5.chunk.css"
+  },
+  {
+    "revision": "fb65b1eec6ffccfdc38b",
+    "url": "/TestProject/static/js/2.c591d78b.chunk.js"
+  },
+  {
+    "revision": "6ffb1809fba552e8302c",
+    "url": "/TestProject/static/js/main.b0d2a40b.chunk.js"
+  },
+  {
+    "revision": "f4c70ab33fcfb8f1ecfb",
+    "url": "/TestProject/static/js/runtime-main.fe4f5580.js"
+  }
+]);
