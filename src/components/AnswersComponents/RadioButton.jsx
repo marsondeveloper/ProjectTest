34900d19e0a0ae06e270a7103a1f4932873589ca
удвщкфвщ <marson4ik2019@gmail.com>
2020-04-08 21:44:58 +0300

    commit update-project

--- src/components/AnswersComponents/RadioButton.jsx
+++ src/components/AnswersComponents/RadioButton.jsx
@@ -14,11 +14,11 @@ const RadioButton = (props) => {
             return null
         }
 
-<<<<<<< HEAD
-        let result = props.answers.map((q, i) => <div key={q.id}><label><input name={props.name} type={props.type} value={q.point} onChange={onChangeRadio}/>{q.answer}
-=======
+
+
+
         let result = props.answers.map(q => <div key={q.id}><label><input name={props.name} type={props.type} value={q.point} onChange={onChangeRadio}/>{q.answer}
->>>>>>> d09f741a9b910e7e70cfd871cf964fc8eb2c8c49
+
         </label></div>);
         return (
             <div>
